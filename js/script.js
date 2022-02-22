042d90675749abb56a9cc2b224f785bbd22a567b
vvolleyball <v.kovalenko@student.csn.khai.edu>
2022-02-22 18:37:57 +0200

    add audio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,42 @@
-const sliderThumbs = new Swiper('.slider-thumbs', {
+// menu open-close
+const burger = document.querySelector('.burger');
+const navigation = document.querySelector('.navigation');
+const navigationClose = document.querySelector('.navigation__close');
+
+burger.addEventListener('click', () => {
+    navigation.classList.add('navigation_active');
+});
+
+navigationClose.addEventListener('click', () => {
+    navigation.classList.remove('navigation_active');
+});
+
+
+// Music(off,on)
+try {
+const mute = document.querySelector('.mute__checkbox');
+const audio = new Audio('audio/waterTower.mp3')
+
+    const checkMute = () => {
+        if (mute.checked) {
+            audio.play().catch(() => {
+                mute.checked = false
+            });
+        } else {
+               audio.pause();
+        }
+    };
+        
+    checkMute();
+    setTimeout(checkMute, 2000)
+    mute.addEventListener('change', checkMute)
+    } catch {
+        console.log('net video')
+}
+
+// Photo slider
+try {
+    const sliderThumbs = new Swiper('.slider-thumbs', {
     loop: true,
     spaceBetween: 20,
     slidesPerView: 3,
@@ -19,3 +57,23 @@ const sliderMain = new Swiper('.slider-main', {
 
 sliderThumbs.controller.control = sliderMain;
 sliderMain.controller.control = sliderThumbs;
+
+const videos = document.querySelectorAll('video');
+
+sliderMain.on('slideChange', () => {
+    for(let i = 0; i < videos.length; i += 1) {
+        videos[i].pause();
+    }
+});
+
+const pagination = document.querySelector('.pagination');
+const paginationButton = document.querySelector('.pagination__arrow');
+
+paginationButton.addEventListener('click', () => {
+    pagination.classList.toggle('pagination_active')
+})
+
+
+} catch {
+    console.log('нет слайдера')
+}
